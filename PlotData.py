d6a8f60157164aed00b52de38148e489f7959fb4
alexandt <alexandt96@gmail.com>
2020-03-30 23:59:01 -0500

    basic interactive plot

--- PlotData.py
+++ PlotData.py
@@ -1,16 +1,24 @@
 import matplotlib.pyplot as plt
 import plotly.express as px
 import pandas as pd
+import plotly.graph_objects as go
+
 f = open('./Team_Data/TeamPCTWinDiffOverTime.txt', 'r')
 lines = f.readlines()
 
 df = pd.DataFrame()
-for line in lines[:4]:
+plotted_line = []
+fig = go.Figure()
+
+for i, line in enumerate(lines):
     split_line = line.split(',')
-    team = split_line[0]
+    team = split_line[0].strip('\'')
+    temp_team = []
+    for word in team.split(): temp_team.append(word.lower().capitalize())
+    team = ' '.join(temp_team)
     data = split_line[1:]
-    x=[]
-    y=[]
+    team_data=[]
+    years=[]
     for point in data:
         coords = point.split(' ')
         while("" in coords) :
@@ -18,13 +26,17 @@ for line in lines[:4]:
         stripped_coords = []
         for c in coords:
             stripped_coords.append(c.strip('()[]\n\' '))
-        x.append(int(stripped_coords[0]))
-        y.append(float(stripped_coords[1]))
+        years.append(int(stripped_coords[0]))
+        team_data.append(float(stripped_coords[1]))
+    if i == 0: df['Years'] = years
+    df[team] = pd.Series(team_data)
+    fig.add_scatter(x=df['Years'],
+    y=df[team],
+    mode='lines+markers',
+    name=team)
+print(df)
+fig.show()
 
-    plt.plot(x, y, label=team)
-plt.legend()
-plt.show()
-
-#     df = df.append({team : x, 'Percent Point Diff' : y}, ignore_index=True)
-#     fig = px.line(df, x=team, y="Percent Point Diff", title='Percent Point Differential by Team')
-# fig.show()
+#     plt.plot(x, y, label=team)
+# plt.legend()
+# plt.show()
